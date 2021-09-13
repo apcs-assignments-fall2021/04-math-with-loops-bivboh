588e540c462f9684599ef79593bff24abd928091
athenian <bivboh@gmail.com>
2021-09-12 21:44:57 -0700

    first attempt

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,8 +4,11 @@ public class MyMain {
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int factorial = 1;
+        for (int i = 1; i <= x; ++i) {
+            factorial = factorial * i;
+        }
+        return factorial;
     }
 
     // Calculates the value for the math constant e iteratively
@@ -18,13 +21,22 @@ public class MyMain {
 
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double n = x;
+        double a = 1;
+        double accuracy = 0.00001;
+        while (n-a > accuracy) {
+            n = (n + a) / 2;
+            a = x/n;
+        }
+        return n;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Pick a number");
+        int x = scan.nextInt();
+        System.out.println("The factorial of your number is: " + factorial(x));
 
         // YOUR CODE HERE
 
